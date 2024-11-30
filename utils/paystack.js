8f8c2c4d45f24e397b1368dc9019b05c3d5e29b5
NathanielWatife <awosanminathaniel0@gmail.com>
2024-11-30 22:17:07 +0100

    Configured paystack and stripe payment

---
+++ utils/paystack.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+require('dotenv').config();
+
+const paystack = axios.create({
+    baseURL: 'https://api.paystack.co',
+    headers: {
+        Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
+    },
+});
+
+const initializePaystackPayment = async ({ email, amount }) => {
+    const response = await paystack.post('/transaction/initialize', {
+        email,
+        amount: amount * 100, // convert to kobi
+        currency: 'NGN',
+    });
+    return response.data; // return payment link and transaction reference
+};
+
+const verifyPaystackPayment = async (reference) => {
+    const response = await paystack.get(`/transaction/verify/${reference}`);
+    return response.data; // return payment status and details
+};
+
+module.exports = { initializePaystackPayment, verifyPaystackPayment };

58755de321de4efea7ae4682a2dd7295ae33da86
NathanielWatife <awosanminathaniel0@gmail.com>
2024-12-14 00:01:11 +0100

    Added middleware to protect routes

---
+++ src/middlewares/auth.js
@@ -0,0 +1,13 @@
+const jwt = require('jwt');
+
+module.exports = (req, res, next) => {
+    const token = req.header('Authorization');
+    if (!token) return res.status(401).json({error: 'No token, authorization denied'});
+    try {
+        const deoded = jwt.verify(token, process.env.JWT_SECRET);
+        res.user = decoded;
+        next();
+    } catch (err) {
+        res.status(401).json({ error: 'Token is not valid' });
+    }
+};
